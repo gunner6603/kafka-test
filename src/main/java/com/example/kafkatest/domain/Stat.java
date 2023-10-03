e326d73f940dfe3190fc28336127b421ddaad328
Geon <gunner6603@snu.ac.kr>
2023-10-03 17:28:32 +0900

    chore: 의존성 추가 및 DB 설정

---
+++ src/main/java/com/example/kafkatest/domain/Stat.java
@@ -0,0 +1,24 @@
+package com.example.kafkatest.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Getter
+public class Stat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String productName;
+
+    private Long count;
+}
+
